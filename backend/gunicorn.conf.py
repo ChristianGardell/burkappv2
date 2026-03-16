cd3ed167297f650ba2f1a0df7b7ee5f2dc9b646e
Christian Gardell <ch0021ga-s@student.lu.se>
2026-03-16 20:01:54 +0100

    New way to deploy, is now containerized,  new log location

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,18 +1,17 @@
 # gunicorn.conf.py
 
 # 1. Network
-bind = "127.0.0.1:8000"
+bind = "0.0.0.0:8000"
 
 # 2. Worker Configuration
 # Using the standard Uvicorn worker
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = 4  # Standard for most small servers (2 * cores + 1)
 
-# 3. Logging
-# These paths MUST match what you created with 'touch' and 'chown'
-accesslog = "/var/log/burkapp_access.log"
-errorlog = "/var/log/burkapp_error.log"
 
+# 3. Logging
+accesslog = "/app/logs/access.log"
+errorlog  = "/app/logs/error.log"
 # capture_output ensures that your print() and logger.info()
 # from the middleware actually end up in the errorlog or accesslog
 capture_output = True
