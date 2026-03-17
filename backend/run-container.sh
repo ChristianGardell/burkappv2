7e2dda1a3921d3331e1e6f929db55ec8ec959f76
t14 <ch0021ga-s@student.lu.se>
2026-03-17 17:34:07 +0000

    fixedx podman persistant db

--- backend/run-container.sh
+++ backend/run-container.sh
@@ -2,8 +2,4 @@
 podman build -t beertrack-backend:latest .
 
 
-podman run \
- --name beertrack \
- -p 8000:8000 \
- -v ~/logs/beertrack-logs:/app/logs:Z \
-  beertrack-backend:latest
+systemctl --user restart beertrack.service
