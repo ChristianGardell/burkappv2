55b891d1f079fc9b7c91b5e5c6440776bc3aaf7c
Christian Gardell <ch0021ga-s@student.lu.se>
2026-03-29 12:49:11 +0200

    Sqlalchemy 2.0 migration, now not needed to cast Column[str] to str

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,22 +1,15 @@
 import uuid
 
-from sqlalchemy import (
-    Boolean,
-    Column,
-    Float,
-    ForeignKey,
-    Integer,
-    String,
-    UniqueConstraint,
-)
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..db.database import Base
 
 
 class Groups(Base):
     __tablename__ = "groups"
-    id = Column(
+
+    id: Mapped[str] = mapped_column(
         String,
         primary_key=True,
         default=lambda: str(uuid.uuid4()),
@@ -24,18 +17,19 @@ class Groups(Base):
         index=True,
         nullable=False,
     )
-    swish_number = Column(String, nullable=True)
-    price_per_beer = Column(Integer, default=10, nullable=False)
-    name = Column(String, nullable=False)
-    invite_code = Column(String, unique=True, nullable=False)
-    members = relationship(
+    swish_number: Mapped[str | None] = mapped_column(String, nullable=True)
+    price_per_beer: Mapped[int] = mapped_column(Integer, default=10, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    invite_code: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    members: Mapped[list["Users"]] = relationship(
         "Users", back_populates="group", cascade="all, delete-orphan"
     )
 
 
 class Users(Base):
     __tablename__ = "users"
-    id = Column(
+
+    id: Mapped[str] = mapped_column(
         String,
         primary_key=True,
         default=lambda: str(uuid.uuid4()),
@@ -43,34 +37,40 @@ class Users(Base):
         index=True,
         nullable=False,
     )
-    beers = Column(Integer, default=0, nullable=False)
-    name = Column(String, nullable=False)
-    phone_number = Column(String, unique=True, index=True, nullable=False)
-    admin = Column(Boolean, default=False, nullable=False)
-    owner = Column(Boolean, default=False, nullable=False)
-    group_id = Column(
+    beers: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    phone_number: Mapped[str] = mapped_column(
+        String, unique=True, index=True, nullable=False
+    )
+    admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    owner: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    group_id: Mapped[str] = mapped_column(
         String, ForeignKey("groups.id", ondelete="CASCADE"), index=True, nullable=False
     )
-    hashed_pin = Column(String, nullable=False)
-    total_beers = Column(Integer, default=0, nullable=False)
-
-    group = relationship("Groups", foreign_keys=[group_id], back_populates="members")
-    beer_log = relationship(
+    hashed_pin: Mapped[str] = mapped_column(String, nullable=False)
+    total_beers: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    group: Mapped["Groups"] = relationship(
+        "Groups", foreign_keys=[group_id], back_populates="members"
+    )
+    beer_log: Mapped[list["BeerLog"]] = relationship(
         "BeerLog", back_populates="user", cascade="all, delete-orphan"
     )
 
 
 class BeerLog(Base):
     __tablename__ = "beer_log"
-    id = Column(
+
+    id: Mapped[str] = mapped_column(
         String,
         primary_key=True,
         default=lambda: str(uuid.uuid4()),
         unique=True,
         index=True,
     )
-    user_id = Column(
+    user_id: Mapped[str] = mapped_column(
         String, ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False
     )
-    timestamp = Column(String, nullable=False)
-    user = relationship("Users", foreign_keys=[user_id], back_populates="beer_log")
+    timestamp: Mapped[str] = mapped_column(String, nullable=False)
+    user: Mapped["Users"] = relationship(
+        "Users", foreign_keys=[user_id], back_populates="beer_log"
+    )
